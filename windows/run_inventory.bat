05522f474330b8bffffd719ef8b6126801be7e47
Badr Eddine BELGHARBI <112476731+badr-spacefoot@users.noreply.github.com>
2025-12-03 15:38:49 +0100

    Update run_inventory.bat

--- windows/run_inventory.bat
+++ windows/run_inventory.bat
@@ -11,7 +11,7 @@ REM Détection du répertoire du script
 set "SCRIPT_DIR=%~dp0"
 
 REM Lancer le launcher PowerShell en UTF-8
-powershell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%inventaire_windows_launcher.ps1"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%inventaire_windows_launcher.ps1"
 
 echo.
 echo ------------------------------------------------

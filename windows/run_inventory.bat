7eb036cc218f8f1765959887c9da9c889f0d596b
Badr Eddine BELGHARBI <112476731+badr-spacefoot@users.noreply.github.com>
2025-12-03 15:47:54 +0100

    Update run_inventory.bat

--- windows/run_inventory.bat
+++ windows/run_inventory.bat
@@ -1,20 +1,4 @@
 @echo off
-chcp 65001 >nul
-
-echo.
-echo ==============================================
-echo   Spacefoot Auto Inventory - Windows Launcher
-echo ==============================================
-echo.
-
-REM Détection du répertoire du script
-set "SCRIPT_DIR=%~dp0"
-
-REM Lancer le launcher PowerShell en UTF-8
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%inventaire_windows_launcher.ps1"
-
-echo.
-echo ------------------------------------------------
-echo   Programme terminé. Appuyez sur une touche...
-echo ------------------------------------------------
-pause >nul
+cd /d "%~dp0"
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%~dp0inventaire_windows_launcher.ps1"
+pause

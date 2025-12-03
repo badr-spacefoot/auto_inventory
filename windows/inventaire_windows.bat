19f12958665ef2831cbcee99b40dd389b48ebf84
Badr Eddine BELGHARBI <112476731+badr-spacefoot@users.noreply.github.com>
2025-12-03 03:25:16 +0100

    Delete windows/inventaire_windows.bat

--- windows/inventaire_windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET SPACEFOOT_INVENTAIRE=1
-SET SPACEFOOT_CONFIGDIR=%~dp0
-
-powershell -ExecutionPolicy Bypass -NoLogo -NoProfile -WindowStyle Normal -File "%~dp0inventaire_windows_core.ps1"
-
-exit
